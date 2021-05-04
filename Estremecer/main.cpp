93b1983d3b101940d51c08c8c873ba1568db6f6f
Rocío García <35541675+rocgar08@users.noreply.github.com>
2021-05-04 12:21:27 +0200

    Add files via upload

---
+++ Estremecer/main.cpp
@@ -0,0 +1,46 @@
+/*
+NOMBRE Y APELLIDOS:
+	Carla Peñarrieta Uribe
+	Rocio García Núñez
+	Grupo 14
+*/
+
+#include <iostream>
+#include "lista.h"
+using namespace std;
+
+bool lee_lista(Lista<int>& lista) {
+	int n_elems;
+	cin >> n_elems;
+	if (n_elems >= 0) {
+		while (n_elems-- != 0) {
+			int elem;
+			cin >> elem;
+			lista.pon_final(elem);
+		}
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool ejecuta_caso() {
+	Lista<int> lista;
+	if (lee_lista(lista)) {
+		lista.estremece();
+		lista.imprime();
+		cout << "#";
+		lista.imprime_inv();
+		cout << endl;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main() {
+	while (ejecuta_caso());
+	return 0;
+}
